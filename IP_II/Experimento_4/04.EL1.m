1bfb15aa50b9be73a6b56bad2338e69a7dc5b3b6
Stefano Suraci <stefanosampaio@hotmail.com>
2023-02-23 16:44:32 -0300

    Add files via upload

---
+++ IP_II/Experimento_4/04.EL1.m
@@ -0,0 +1,58 @@
+pkg load statistics
+warning ("off");
+t0 = clock ();
+experiment = 2       #scenarios of experiment "2" or "3": change this number of the experiment here! 
+qtd_itr=50000                             ### (qtd_itr por qtt_out)
+qtt_outs_min = 0                          
+qtt_outs_max = 1                          
+pesos_aux = {'ppind','ppcor', 'prind', 'prcor', 'decLS'};      
+alias_aux = {'6obserip2'};
+test_stat_aux = {'sig_obs'};  
+for alias_num = 1:length(alias_aux)
+ for test_stat_num = 1:length(test_stat_aux)         
+  for pesos_num = 1:length(pesos_aux)
+   for min_err = [3, 6]                   
+   
+    mfilename ()
+    pesos = pesos_aux{pesos_num}
+    alias = alias_aux{alias_num}
+    test_stat = test_stat_aux{test_stat_num}
+    min_err
+    max_err = min_err + 3               
+    WL1 = 0;
+
+filename=sprintf("VC_L1_%s_%s_%s_%d_experiment%d", pesos, alias, test_stat, qtd_itr,experiment);
+VCAll=importdata(filename);
+alpha=VCAll(8,1)
+WL1=VCAll(8,2)  
+
+
+[A, dp, MVC, P, U, m, n] = get_network(alias, experiment);
+
+
+filename=sprintf("%s_vAll_L1_%s_%d-%douts_%d-%ds_%ditr_experiment%d", alias,... 
+pesos, qtt_outs_min, qtt_outs_max, min_err, max_err, qtd_itr, experiment);
+vAll=importdata(filename);
+
+if qtt_outs_max != 0
+filename=sprintf("%s_outs_positions_%d-%douts_%d-%ds_%ditr_experiment%d", alias,... 
+qtt_outs_min, qtt_outs_max, min_err, max_err, qtd_itr, experiment);
+y_val=importdata(filename);
+else y_val=zeros(qtd_itr,m); endif
+
+if test_stat == "sigma_v"
+  y_pred = abs(vAll./DPvv) > WL1;
+  else y_pred = abs(vAll./dp) > WL1;end
+
+[acc, y_pred_acertou] = acc_score(y_pred,y_val);
+acc
+
+filename=sprintf("acc_EL1_%s_%s_%s_%d-%douts_%d-%ds_%d_alpha%d_experiment%d", pesos, alias,...
+test_stat, qtt_outs_min, qtt_outs_max, min_err, max_err, qtd_itr, alpha*100, experiment);
+save ("-ascii", filename, "acc");
+
+tempo = etime (clock (), t0)/60
+
+
+
+endfor endfor endfor endfor
---
+++ IP_II/Experimento_4/get_network.m
@@ -0,0 +1,43 @@
+function [A, dp, MVC, P, U, m, n] = get_network(alias, experiment)
+
+if alias == "6obserip2"
+pkg load statistics
+pkg load optim
+rand("state",[1]);randn("state",[1]);
+
+A=[1 0 0;-1 1 0;0 -1 1;0 0 -1; 0 -1 0; -1 0 1];
+
+[m n]=size(A);
+
+aux = rand(m,m);
+if experiment == 3
+MVC =  5-10*aux;endif 
+  
+if experiment == 2
+MVC =  1-2*aux;endif
+
+
+for i=2:m
+  for j=1:(i-1)
+  MVC(j,i)=MVC(i,j); end end 
+  
+  
+for i=1:m
+  MVC(i,i)=5+2.5*i; 
+end 
+
+if experiment == 1 
+MVC =  zeros(m,m); 
+for i=1:m
+  MVC(i,i)=5+2.5*i;end 
+endif
+endif
+
+
+
+#############################################################################
+dp=sqrt(diag(MVC))';
+[m n]=size(A);
+P = inv(MVC);
+U = chol(P);
+endfunction
